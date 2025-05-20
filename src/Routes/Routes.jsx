8138e2bfb2f016ea6b957ff6859dca9930a71975
amir811537 <amirhossain.bc.75@gmail.com>
2025-05-20 15:36:42 +0600

    added new

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,25 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Mainlayout from "../Layouts/Mainlayout";
-import Home2 from "../Components/pages/Home2";
-import InputLayout from "../Components/pages/InputLayout";
+import CustomerDetail from "../Components/pages/CustomerDetail";
+import CmsHome from "../Components/pages/CmsHome";
 
-
-
-const router=createBrowserRouter([
-    {
-        path:'/',
-        element:<Mainlayout></Mainlayout>,
-        children:[
-            {
-                path:'/dwonloadpage',
-                element:<Home2></Home2>
-            },
-            {
-                path:"/",
-                element:<InputLayout></InputLayout>
-            }
-            
-        ]
-    }
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Mainlayout />,
+    children: [
+      {
+        index: true, // 👈 Default route ("/") will render Home
+        element:<CmsHome></CmsHome>
+      },
+      {
+        path: "home", // 👈 Optional "/home" route (e.g. /home)
+        element: <CmsHome />,
+      },
+      {
+        path: "customerDetails",
+        element: <CustomerDetail />,
+      },
+    ],
+  },
 ]);
+
 export default router;
